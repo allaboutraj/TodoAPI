ca611eddff687090fd8079705b0b508584ba6e6a
allaboutraj <rajgupta9507321@gmail.com>
2024-03-29 01:10:38 +0530

    Added Logic for Service layer

--- src/services/todo.service.js
+++ src/services/todo.service.js
@@ -1,20 +1,21 @@
-class TodoService{
-    constructor(todoRepository){
+class TodoService {
+
+    constructor(todoRepository) {
         this.todoRepository = todoRepository;
     }
 
-    create(todoText){
-        //Business Logic
+    create(todoText) {
+        // an BL u want to exec
         if(todoText.length == 0) return;
         todoText = todoText.trim().toUpperCase();
         this.todoRepository.insert(todoText);
     }
 
-    getOneTodo(id){
+    getOneTodo(id) {
         return this.todoRepository.get(id);
     }
 
-    getAllTodos(){
+    getAllTodos() {
         return this.todoRepository.getAll();
     }
 }
