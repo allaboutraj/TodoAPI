11462502252e0ffdfa63ae1df41874a3eb530b1c
allaboutraj <rajgupta9507321@gmail.com>
2024-03-29 01:26:48 +0530

    Debugging

--- src/services/todo.service.js
+++ src/services/todo.service.js
@@ -5,7 +5,6 @@ class TodoService {
     }
 
     create(todoText) {
-        // an BL u want to exec
         if(todoText.length == 0) return;
         todoText = todoText.trim().toUpperCase();
         this.todoRepository.insert(todoText);
